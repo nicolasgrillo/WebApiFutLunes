32218c5a9ad47eb81e028fd112cd5d6f9af33d07
Grillo, Nicolás Alfredo <nicolas.a.grillo@accenture.com>
2018-08-06 21:38:21 -0300

    Updating connection

--- WebApiFutLunes/Models/IdentityModels.cs
+++ WebApiFutLunes/Models/IdentityModels.cs
@@ -20,7 +20,7 @@ namespace WebApiFutLunes.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("FutLunesDb", throwIfV1Schema: false)
         {
         }
         
