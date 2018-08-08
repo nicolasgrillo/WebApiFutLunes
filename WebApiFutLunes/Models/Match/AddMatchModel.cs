4e6ffb3b08e1fcaf941a7b2e80bb76ac4d831360
Grillo, Nicolás Alfredo <nicolas.a.grillo@accenture.com>
2018-08-08 09:02:57 -0300

    Adding AddMatchModel

---
+++ WebApiFutLunes/Models/Match/AddMatchModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebApiFutLunes.Models.Match
+{
+    public class AddMatchModel
+    {
+        public string LocationTitle { get; set; }
+        public string LocationMapUrl { get; set; }
+        public DateTime MatchDate { get; set; }
+        public int PlayerLimit { get; set; }
+    }
+}
