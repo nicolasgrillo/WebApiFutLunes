90a338cef25c29e4ed2595f1a45c71698b20e080
Grillo, Nicolás Alfredo <nicolas.a.grillo@accenture.com>
2018-08-08 09:32:21 -0300

    Annotating ID field for Match entity

--- WebApiFutLunes.Data/Entities/Match.cs
+++ WebApiFutLunes.Data/Entities/Match.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +11,8 @@ namespace WebApiFutLunes.Data.Entities
 {
     public class Match
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string LocationTitle { get; set; }
         public string LocationMapUrl { get; set; }
