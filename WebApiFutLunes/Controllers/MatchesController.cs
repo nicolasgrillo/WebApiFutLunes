ef17a4ee6930f337237f661acb131a70ab7c1d05
Grillo, Nicolás Alfredo <nicolas.a.grillo@accenture.com>
2018-08-08 10:24:43 -0300

    Adding MatchesController

---
+++ WebApiFutLunes/Controllers/MatchesController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Ajax.Utilities;
+using WebApiFutLunes.Data.Contexts;
+using WebApiFutLunes.Data.Entities;
+using WebApiFutLunes.Data.Models;
+using WebApiFutLunes.Models.Match;
+
+namespace WebApiFutLunes.Controllers
+{
+    public class MatchesController : ApiController
+    {
+        private ApplicationDbContext _context { get; set; }
+
+        public MatchesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public async Task<IHttpActionResult> Get()
+        {
+            var result = await _context.Matches.ToListAsync();
+            if (!result.Any())
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+        public async Task<IHttpActionResult> Get(int id)
+        {
+            var result = await _context.Matches.FindAsync(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+        // Add new match
+        // POST api/matches
+        public async Task<IHttpActionResult> Post([FromBody] AddMatchModel matchModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var match = new Match()
+            {
+                LocationMapUrl = matchModel.LocationMapUrl,
+                LocationTitle = matchModel.LocationTitle,
+                MatchDate = matchModel.MatchDate,
+                PlayerLimit = matchModel.PlayerLimit,
+                Players = new List<ApplicationUser>()
+            };
+
+            _context.Matches.Add(match);
+            if (await _context.SaveChangesAsync() <= 0)
+            {
+                return InternalServerError();
+            }
+            return Ok(match);
+        }
+    }
+}
