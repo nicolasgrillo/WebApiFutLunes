c9b94ae6b567d77d37a731bd02f42d3f82818633
Grillo, Nicolás Alfredo <nicolas.a.grillo@accenture.com>
2018-08-08 00:53:13 -0300

    Adding Match entity

---
+++ WebApiFutLunes.Data/Entities/Match.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiFutLunes.Data.Models;
+
+namespace WebApiFutLunes.Data.Entities
+{
+    public class Match
+    {
+        public int Id { get; set; }
+        public string LocationTitle { get; set; }
+        public string LocationMapUrl { get; set; }
+        public DateTime MatchDate { get; set; }
+        public int PlayerLimit { get; set; }
+        public ICollection<ApplicationUser> Players { get; set; }
+    }
+}
