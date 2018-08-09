b3fbd3480223bfe4674a43c1013092992ce08446
Grillo, Nicolás Alfredo <nicolas.a.grillo@accenture.com>
2018-08-09 08:50:11 -0300

    Adding PlayersController with GET action

---
+++ WebApiFutLunes/Controllers/PlayersController.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.Entity;
+using System.Threading.Tasks;
+using System.Web.Http;
+using WebApiFutLunes.Data.Contexts;
+
+namespace WebApiFutLunes.Controllers
+{
+    public class PlayersController : ApiController
+    {
+        private ApplicationDbContext _context { get; set; }
+
+        public PlayersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public async Task<IHttpActionResult> Get()
+        {
+            var players = await _context.Users.ToListAsync();
+            if (players == null)
+            {
+                return NotFound();
+            }
+            return Ok(players);
+        }
+    }
+}
