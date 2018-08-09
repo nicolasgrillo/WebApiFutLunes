fd283e48de702ff4b6bfc7b9f2a3feeaa37e8a13
Grillo, Nicolás Alfredo <nicolas.a.grillo@accenture.com>
2018-08-09 15:17:39 -0300

    Adding UserSubscription Dto

---
+++ WebApiFutLunes.Data/DTOs/UserSubscription.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiFutLunes.Data.DTOs
+{
+    public class UserSubscription
+    {
+        public ApplicationUser User { get; set; }
+        public DateTime SubscriptionDate { get; set; }
+    }
+}
