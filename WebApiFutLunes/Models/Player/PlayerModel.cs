feed062445d1815e6147ec968db639a3613677e9
Grillo, Nicolás Alfredo <nicolas.a.grillo@accenture.com>
2018-08-09 08:52:28 -0300

    Adding PlayerModel

---
+++ WebApiFutLunes/Models/Player/PlayerModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiFutLunes.Models.Player
+{
+    public class PlayerModel
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public int Appearances { get; set; }
+    }
+}
