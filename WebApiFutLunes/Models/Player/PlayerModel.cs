3dafeddf28f276b5afc842c059138df6801d91a9
Grillo, Nicolás Alfredo <nicolas.a.grillo@accenture.com>
2018-08-09 11:43:09 -0300

    Updating PlayerModel field's name - EmailAddress to Email

--- WebApiFutLunes/Models/Player/PlayerModel.cs
+++ WebApiFutLunes/Models/Player/PlayerModel.cs
@@ -10,7 +10,7 @@ namespace WebApiFutLunes.Models.Player
         public string UserName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string EmailAddress { get; set; }
+        public string Email { get; set; }
         public int Appearances { get; set; }
     }
 }
