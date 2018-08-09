1f59fe40b4459ea8efab90b9a52a8fc401fddc6b
Grillo, Nicolás Alfredo <nicolas.a.grillo@accenture.com>
2018-08-09 15:58:21 -0300

    Database Restore

--- WebApiFutLunes.Data/Migrations/Configuration.cs
+++ WebApiFutLunes.Data/Migrations/Configuration.cs
@@ -1,30 +1,23 @@
-using System.Data.Entity.Migrations;
-using WebApiFutLunes.Data.Contexts;
-
 namespace WebApiFutLunes.Data.Migrations
 {
-    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<WebApiFutLunes.Data.Contexts.ApplicationDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
-            ContextKey = "WebApiFutLunes.Data.Contexts.ApplicationDbContext";
         }
 
-        protected override void Seed(ApplicationDbContext context)
+        protected override void Seed(WebApiFutLunes.Data.Contexts.ApplicationDbContext context)
         {
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            //  to avoid creating duplicate seed data.
         }
     }
 }
