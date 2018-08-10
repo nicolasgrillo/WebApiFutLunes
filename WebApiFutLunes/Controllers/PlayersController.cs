89c9bc1a71aba24604b37838754f24497e344283
Grillo, Nicolás Alfredo <nicolas.a.grillo@accenture.com>
2018-08-10 15:29:42 -0300

    Implementing IoC - PlayersController

--- WebApiFutLunes/Controllers/PlayersController.cs
+++ WebApiFutLunes/Controllers/PlayersController.cs
@@ -1,11 +1,10 @@
 ﻿using System.Collections.Generic;
-using System.Data.Entity;
 using System.Threading.Tasks;
 using System.Web.Http;
 using AutoMapper;
 using WebApiFutLunes.Data.Contexts;
 using WebApiFutLunes.Data.DTOs;
-using WebApiFutLunes.Data.Repositories;
+using WebApiFutLunes.Data.Repositories.Interface;
 using WebApiFutLunes.Models.Player;
 
 namespace WebApiFutLunes.Controllers
@@ -13,12 +12,11 @@ namespace WebApiFutLunes.Controllers
     public class PlayersController : ApiController
     {
         private ApplicationDbContext _context { get; set; }
-        private PlayersRepository _repo { get; set; }
+        private IPlayersRepository _repo { get; set; }
 
-        public PlayersController()
+        public PlayersController(IPlayersRepository repo)
         {
-            _context = new ApplicationDbContext();
-            _repo = new PlayersRepository(_context);
+            _repo = repo;
         }
 
         public async Task<IHttpActionResult> Get()
