7ac0b097dfee4e333b750ab2ba73b5182d3d6002
Grillo, Nicolás Alfredo <nicolas.a.grillo@accenture.com>
2018-08-10 11:18:31 -0300

    Adding Get TestMethod

--- WebApiFutLunes.Test/Controllers/MatchesTest.cs
+++ WebApiFutLunes.Test/Controllers/MatchesTest.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Results;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApiFutLunes.Controllers;
+using WebApiFutLunes.Data.Entities;
 
 namespace WebApiFutLunes.Test.Controllers
 {
@@ -6,8 +13,27 @@ namespace WebApiFutLunes.Test.Controllers
     public class MatchesTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public async Task GetReturnsOkOrNotFound()
         {
+            //Arrange
+
+            var controller = new MatchesController
+            {
+                Request = new HttpRequestMessage(),
+                Configuration = new HttpConfiguration()
+            };
+
+            //Act
+
+            IHttpActionResult response = await controller.Get(1);
+            Type respType = response.GetType();
+
+            //Assert
+
+            Assert.IsTrue(
+                respType == typeof(OkResult) ||
+                respType == typeof(NotFoundResult)
+            );
         }
     }
 }
