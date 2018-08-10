3dce92695c0b42d74ff31f38faa0c4f581577cd6
Grillo, Nicolás Alfredo <nicolas.a.grillo@accenture.com>
2018-08-10 12:18:27 -0300

    Add Match test

--- WebApiFutLunes.Test/Controllers/MatchesTest.cs
+++ WebApiFutLunes.Test/Controllers/MatchesTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
@@ -6,6 +7,7 @@ using System.Web.Http.Results;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WebApiFutLunes.Controllers;
 using WebApiFutLunes.Data.Entities;
+using WebApiFutLunes.Models.Match;
 
 namespace WebApiFutLunes.Test.Controllers
 {
@@ -60,6 +62,33 @@ namespace WebApiFutLunes.Test.Controllers
                 respType == typeof(OkResult) ||
                 respType == typeof(NotFoundResult)
             );
+        }
+
+        [TestMethod]
+        [TestCategory("MatchesTests")]
+        public async Task AddMatchReturnsCreated()
+        {
+            //Arrange
+
+            var controller = new MatchesController
+            {
+                Request = new HttpRequestMessage(),
+                Configuration = new HttpConfiguration()
+            };
+
+            //Act
+
+            IHttpActionResult response = await controller.Post(new AddUpdateMatchModel()
+            {
+                LocationTitle = "TestMatch",
+                LocationMapUrl = "TestMatchMapUrl",
+                MatchDate = DateTime.Now,
+                PlayerLimit = 10
+            });
+            
+            //Assert
+            
+            Assert.IsNotInstanceOfType(response.GetType(), typeof(InternalServerErrorResult));
         }
     }
 }
