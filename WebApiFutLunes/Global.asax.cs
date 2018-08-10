ee680743e79d910852e96e9849424a1760c34e11
Grillo, Nicolás Alfredo <nicolas.a.grillo@accenture.com>
2018-08-10 14:44:27 -0300

    Referencing Ninject.Web.WebApi.WebHost - Including in AppStart

--- WebApiFutLunes/Global.asax.cs
+++ WebApiFutLunes/Global.asax.cs
@@ -1,10 +1,12 @@
 ﻿using System.Web.Http;
 using System.Web.Mvc;
+using Ninject;
+using Ninject.Web.Common.WebHost;
 using WebApiFutLunes.Helpers.AutoMapper;
 
 namespace WebApiFutLunes
 {
-    public class WebApiApplication : System.Web.HttpApplication
+    public class WebApiApplication : NinjectHttpApplication
     {
         protected void Application_Start()
         {
@@ -12,6 +14,18 @@ namespace WebApiFutLunes
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             AutoMapperConfiguration.Configure();
+        }
+
+        protected override IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            RegisterServices(kernel);
+            return kernel;
+        }
+
+        private void RegisterServices(IKernel kernel)
+        {
+            // e.g. kernel.Load(Assembly.GetExecutingAssembly());
         }
     }
 }
