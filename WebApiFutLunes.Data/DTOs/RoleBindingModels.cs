ba0397e64b311aa6b89a8736304beb80a49c8250
Grillo, Nicolás Alfredo <nicolas.a.grillo@accenture.com>
2018-08-10 17:11:55 -0300

    Adding RoleBindingModels

---
+++ WebApiFutLunes.Data/DTOs/RoleBindingModels.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiFutLunes.Data.DTOs
+{
+    public class RoleBindingModel
+    {
+        [Required]
+        [StringLength(256, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 2)]
+        [Display(Name = "Role Name")]
+        public string Name { get; set; }
+    }
+
+    public class UsersInRoleModel
+    {
+        public string Id { get; set; }
+        public List<string> EnrolledUsers { get; set; }
+        public List<string> RemovedUsers { get; set; }
+    }
+}
