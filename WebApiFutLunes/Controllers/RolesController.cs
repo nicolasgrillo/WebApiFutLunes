a4423dcfd1d5b6febf10aad79c311bed880db34b
Grillo, Nicolás Alfredo <nicolas.a.grillo@accenture.com>
2018-08-10 17:08:46 -0300

    Adding RolesController

---
+++ WebApiFutLunes/Controllers/RolesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace WebApiFutLunes.Controllers
+{
+    [Authorize(Roles="Admin")]
+    [RoutePrefix("api/Roles")]
+    public class RolesController : ApiController
+    {
+        private ApplicationRoleManager _AppRoleManager;
+
+        protected ApplicationRoleManager AppRoleManager
+        {
+            get
+            {
+                return _AppRoleManager ?? Request.GetOwinContext().GetUserManager<ApplicationRoleManager>();
+            }
+            private set { _AppRoleManager = value; }
+        }
+
+    }
+}
