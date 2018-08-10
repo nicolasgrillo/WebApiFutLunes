f8bd107dcf87bf6e2901caed2757d8a6e466cbd7
Grillo, Nicolás Alfredo <nicolas.a.grillo@accenture.com>
2018-08-10 17:18:30 -0300

    Adding ModelFactory

---
+++ WebApiFutLunes.Data/DTOs/RoleModelFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Routing;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace WebApiFutLunes.Data.DTOs
+{
+    public class ModelFactory
+    {
+        private UrlHelper _UrlHelper;
+
+        public RoleReturnModel Create(IdentityRole appRole)
+        {
+            return new RoleReturnModel
+            {
+                Url = _UrlHelper.Link("GetRoleById", new { id = appRole.Id}),
+                Id = appRole.Id,
+                Name = appRole.Name
+            };
+        }
+    }
+
+    public class RoleReturnModel
+    {
+        public string Url { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
